9f0e318f8ed5da0d5036b12235d0bc0cc53ea8ab
LyceumYandex <48757285+LyceumYandex@users.noreply.github.com>
2019-10-15 16:31:58 +0300

    Fix bug with Button and Change names buttons
    Был баг при нажатии на кнопку сохранить путь он сробатывала как проверить - исправил, поменял название кнопок

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import os
 import core
+
+
 class Ui_MainWindow(object):
     def __init__(self):
         self.jpg_files = []
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(370, 246)
@@ -48,20 +51,30 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.pushButton_3.clicked.connect(self.connect_for_pushButton_debug1)
         self.pushButton.clicked.connect(self.connect_for_pushButton_debug)
-        self.pushButton.clicked.connect(self.connect_for_pushButton_debug1)
         self.pushButton_5.clicked.connect(self.connect_for_pushButton_debug1)
         self.pushButton_4.clicked.connect(self.connect_for_pushButton_debug)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "1"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Тестировщик работ"))
+        self.pushButton.setText(_translate("MainWindow", "Сохранить путь"))
         self.pushButton_2.setText(_translate("MainWindow", "2"))
-        self.pushButton_3.setText(_translate("MainWindow", "3"))
+        self.pushButton_3.setText(_translate("MainWindow", "Проверить работы"))
         self.label.setText(_translate("MainWindow", "Путь к работам учасников"))
         self.label_2.setText(_translate("MainWindow", "Путь куда будут сохранены ответы учасников"))
         self.pushButton_4.setText(_translate("MainWindow", "debug"))
         self.pushButton_5.setText(_translate("MainWindow", "debug1"))
+
+        self.pushButton_3.move(40, 170)
+
+        self.pushButton.resize(self.pushButton.sizeHint())
+        self.pushButton_2.resize(self.pushButton_2.sizeHint())
+        self.pushButton_3.resize(self.pushButton_3.sizeHint())
+        self.pushButton_4.resize(self.pushButton_4.sizeHint())
+        self.pushButton_5.resize(self.pushButton_5.sizeHint())
+
     def connect_for_pushButton_debug(self, MainWindow):
 
         global files
@@ -81,10 +94,13 @@ class Ui_MainWindow(object):
         files = []
         file_path = ''
         pass
+
     def connect_for_pushButton_debug1(self, MainWindow):
         for i in self.jpg_files:
             print(f"{self.file_path}/{i}")
             core.core(f"{self.file_path}/{i}")
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
